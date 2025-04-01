832d7b201a880fc6e0ce0484828c2efc15d855d5
ADMIN <ADMIN@DESKTOP-1J2SB87>
2025-04-01 22:29:27 +0700

    Add project files.

---
+++ WebCTDL/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using startup.Models;
+namespace WebCTDL.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+        public DbSet<Menu> Menus { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ WebCTDL/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCTDL.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Lấy connection string trước khi dùng
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Đăng ký DbContext trước khi gọi builder.Build()
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
